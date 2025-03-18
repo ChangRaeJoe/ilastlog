82b4c547170f835e376cb1267956ebf0edd86de6
jch <ckdfo1995@gmail.com>
2025-03-18 11:54:18 +0900

    fix: Resolve file saving bug by adding a new module

--- src/lib/fileManger.ts
+++ src/lib/fileManger.ts
@@ -1,32 +1,31 @@
 import {loadJsonFile, loadJsonFileSync} from "load-json-file";
 import {writeJsonFile, writeJsonFileSync} from "write-json-file";
-import {access, copyFile, constants} from "node:fs/promises";
-import {accessSync, copyFileSync} from "node:fs";
-import {rootPath} from "get-root-path";
+import {access, constants} from "node:fs/promises";
+import {accessSync} from "node:fs";
 import path from "node:path";
+import envPaths from "env-paths";
 // eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
 export interface Data {
     [idx: string]: string;
 }
 
-const filePath = path.join(rootPath, "data", "lastlog.json");
-const baseFilePath = path.join(rootPath, "data", "emptyLastlog.json");
+const fileName = "lastlog.json";
+const paths = envPaths("ilastlog");
+const basePath = path.join(paths.data, fileName);
 const mode = 0o666;
 
 export function appendFile(newData: Data): void {
     let data: Data = {};
     try {
-        accessSync(filePath, constants.F_OK);
+        accessSync(basePath, constants.F_OK);
+        data = loadJsonFileSync(basePath, {}) as Data;
     } catch (err) {
-        copyFileSync(baseFilePath, filePath, constants.COPYFILE_EXCL);
+        data = {};
     }
-
-    data = loadJsonFileSync(filePath, {}) as Data;
-
     data = Object.assign(data, newData);
 
     try {
-        writeJsonFileSync(filePath, data);
+        writeJsonFileSync(basePath, data);
     } catch (err) {
         throw new Error("Failed to Write:" + err);
     }
@@ -36,17 +35,15 @@ export async function appendFileAsync(newData: Data) {
     let data: Data = {};
 
     try {
-        await access(filePath, constants.F_OK);
+        await access(basePath, constants.F_OK);
+        data = (await loadJsonFile(basePath, {})) as Data;
     } catch (error) {
-        return copyFile(baseFilePath, filePath, constants.COPYFILE_EXCL);
+        data = {};
     }
-
-    data = (await loadJsonFile(filePath, {})) as Data;
-
     data = Object.assign(data, newData);
 
     try {
-        await writeJsonFile(filePath, data);
+        await writeJsonFile(basePath, data);
     } catch (error) {
         throw new Error("Failed to Write:" + error);
     }
